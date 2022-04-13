9a8354186a0ef7de214b6519741b2abf09de97b2
Christopher Melen <christopher.melen@rncm.ac.uk>
2022-04-13 06:10:20 +0100

    Now building to the project root, so the index.html visible to GitHub Pages can see the bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    //publicPath: '/' //'/dist/'
+    path: path.resolve(__dirname), //path.resolve(__dirname, 'public'),
+    publicPath: '/' //'/dist/'
   },
   module: {
     rules: [
