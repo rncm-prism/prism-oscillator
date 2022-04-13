3143fba5da7ea1f464907b59547c28cf7dfde99c
Christopher Melen <christopher.melen@rncm.ac.uk>
2022-04-13 04:32:10 +0100

    Moved bundle.js to top level of public folder.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, 'public'),
     publicPath: '/dist/'
   },
   module: {
