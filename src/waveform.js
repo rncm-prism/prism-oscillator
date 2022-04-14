c86f3ee86372f7e737c140310f5459260c438352
Christopher Melen <christopher.melen@rncm.ac.uk>
2022-04-14 17:31:23 +0100

    Added some logic to the waveform rendering code to ensure that it is rendered only after a few iterations of requestAnimationFrame have passed - the waveform buffer will be initialised to zero for the first few iterations, which renders the waveform as a centre line, but slightly thicker. We don't want this, so skip the first few renders.

--- src/waveform.js
+++ src/waveform.js
@@ -1,17 +1,33 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { osc } from "./oscillator";
 
+
+// https://medium.com/wdstack/fixing-html5-2d-canvas-blur-8ebe27db07da
+const resizeCanvas = (canvas) => {
+  // Get DPI...
+  const dpi = window.devicePixelRatio;
+
+  // Get computed width and height...
+  const computedStyle = window.getComputedStyle(canvas, null);
+  const width = +computedStyle.getPropertyValue('width').slice(0,-2);
+  const height = +computedStyle.getPropertyValue('height').slice(0,-2);
+
+  // Set the new width and height attributes.
+  canvas.setAttribute('width', width * dpi);
+  canvas.setAttribute('height', height * dpi);
+}
 
 const Waveform = ({ hasAudio }) => {
   const canvasRef = useRef();
   const requestRef = useRef();
+  const renderCnt = useRef(0);
 
   const drawCentreLine = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const w = canvas.width
     const h = canvas.height
-    ctx.lineWidth = 0.25;
+    ctx.lineWidth = 1;
     ctx.beginPath();
     ctx.moveTo(0, h/2);
     ctx.lineTo(w, h/2);
@@ -32,25 +48,37 @@ const Waveform = ({ hasAudio }) => {
 
     drawCentreLine();
   
-    const waveform = osc.getWaveform();
+    if (renderCnt.current >= 7) {
+
+      const waveform = osc.getWaveform();
+
+      ctx.lineWidth = 2.5 //w / 300;
+      ctx.strokeStyle = '#5661FA';
+      ctx.beginPath();
   
-    ctx.lineWidth = w / 300; //1;
-    ctx.strokeStyle = '#5661FA';
-    ctx.beginPath();
-  
-    const sc =  w / waveform.length;
+      const sc =  w / waveform.length;
     
-    for (let i = 0; i < waveform.length; i++) {
-      const x = i * sc;
-      const y = ( 0.5 + (waveform[i] / 2) ) * h;
-      if (i == 0) {
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
+      for (let i = 0; i < waveform.length; i++) {
+        const x = i * sc;
+        const y = ( 0.5 + (waveform[i] / 2) ) * h;
+        if (i == 0) {
+          ctx.moveTo(x, y);
+        } else {
+          ctx.lineTo(x, y);
+        }
       }
+      ctx.stroke();
+
+    } else {
+      renderCnt.current = renderCnt.current + 1;
     }
-    ctx.stroke();
   }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    resizeCanvas(canvas);
+    //window.addEventListener('resize', () => resizeCanvas(canvas));
+  },[]);
 
   useLayoutEffect(() => {
     const canvas = canvasRef.current;
